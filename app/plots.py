c949363ecc03088c7ae3c42c9160a7f6a0eb21ed
Jeff Daley <daley.jeff@gmail.com>
2018-03-05 13:41:30 -0500

    added pick efficiency visualization

---
+++ app/plots.py
@@ -0,0 +1,86 @@
+# code for various visualizations 
+import pandas as pd
+
+from bokeh.models import (
+	ColumnDataSource,
+	HoverTool,
+	LinearColorMapper,
+	BasicTicker,
+	PrintfTickFormatter,
+	ColorBar,
+	Range1d
+	)
+from bokeh.plotting import figure
+from bokeh.embed import components
+
+# draws a heatmap of draft success
+# can be total team value (type='sum'), team pick efficiency (type='mean'),
+# or position (type = 'pos')
+def DraftHeatmap(type):
+
+	start = 2010
+	end = 2017
+	x_range = Range1d(start-0.5, end+0.5, bounds=(1990-0.5, 2017+0.5))
+
+	
+	# TODO: maybe don't assume data files exist lol
+	# TODO: set date range properly 
+	if type == 'mean':
+		df = pd.read_csv('data/draftRVMean.data')
+		title = "NFL Draft Team Pick Efficiency ({0} - {1})".format(1990, 2017)
+	else:
+		df = pd.read_csv('data/draftRVSum.data')
+		title = "NFL Draft Team Success ({0} - {1})".format(1990, 2017)
+
+	# make bokeh happy
+	df.Year = df.Year.astype(str)
+
+	# setup the plot
+	years = list(df.Year.unique())
+	teams = list(df.Team.unique())
+
+	colors = ["#75968f", "#a5bab7", "#c9d9d3", "#e2e2e2", "#dfccce", "#ddb7b1", "#cc7878", "#933b41", "#550b1d"]
+	mapper = LinearColorMapper(palette=colors, low=df.RV.min(), high=df.RV.max())
+
+	source = ColumnDataSource(df)
+
+	TOOLS = "hover,save,xpan,box_zoom,reset,wheel_zoom"
+	
+	p = figure(title=title,
+          x_range = x_range, y_range = list(reversed(teams)),
+          x_axis_location="above", plot_width=600, plot_height=600,
+          tools=TOOLS, toolbar_location="below")
+	p.grid.grid_line_color = None
+	p.axis.axis_line_color = None
+	p.axis.major_tick_line_color = None
+	p.axis.minor_tick_line_color = None
+	p.axis.major_label_text_font_size = "10pt"
+	p.axis.major_label_standoff = 0
+
+
+	p.rect(x="Year", y="Team", width=1, height=1,
+		source=source,
+		fill_color={'field': 'RV', 'transform': mapper},
+		line_color=None)
+
+	color_bar = ColorBar(color_mapper=mapper, major_label_text_font_size="5pt",
+		ticker=BasicTicker(desired_num_ticks=len(colors)),
+		formatter=PrintfTickFormatter(format="%d"),
+		label_standoff=6, border_line_color=None, location=(0,0))
+	p.add_layout(color_bar, 'right')
+
+	# TODO: do I have to if-else this? 
+	if type == 'mean':
+		p.select_one(HoverTool).tooltips = [
+			('Team', '@Team'),
+			('Year', '@Year'),
+			('Total Value', '@RV'),
+		]	
+	else:
+		p.select_one(HoverTool).tooltips = [
+			('Team', '@Team'),
+			('Year', '@Year'),
+			('Total Value', '@RV'),
+		]
+
+	return p
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from app import app, db
 from app.forms import LoginForm, RegistrationForm, PlotChoiceForm
 from app.models import User
+from app.plots import DraftHeatmap
 
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
@@ -116,59 +117,12 @@ def visualizations():
 def vis_draft():
 	form = PlotChoiceForm()
 
-	# TODO: custom start and end here
-	# TODO: put plot stuff in its own function
+	if form.validate_on_submit():
+		choice = form.plotchoice.data
+	else:
+		choice = 'sum'
 
-	start = 2010
-	end = 2017
-	x_range = Range1d(start-0.5, end+0.5, bounds=(1990-0.5, 2017+0.5))
-
-	# draft_vis.data is preformatted to display properly.
-	# TODO: code to build it if it doesn't already exist
-	df = pd.read_csv('data/draftRVSum.data')
-
-	# unfortunately have to change a type to make bokeh happy
-	df.Year = df.Year.astype(str)
-
-	# setup the plot
-	years = list(df.Year.unique())
-	teams = list(df.Team.unique())
-
-	colors = ["#75968f", "#a5bab7", "#c9d9d3", "#e2e2e2", "#dfccce", "#ddb7b1", "#cc7878", "#933b41", "#550b1d"]
-	mapper = LinearColorMapper(palette=colors, low=df.SumRV.min(), high=df.SumRV.max())
-
-	source = ColumnDataSource(df)
-
-	TOOLS = "hover,save,xpan,box_zoom,reset,wheel_zoom"
-
-	# TODO: properly set title
-	p = figure(title="NFL Draft Performance ({0} - {1})".format(1990, 2017),
-          x_range = x_range, y_range = list(reversed(teams)),
-          x_axis_location="above", plot_width=600, plot_height=600,
-          tools=TOOLS, toolbar_location="below")
-	p.grid.grid_line_color = None
-	p.axis.axis_line_color = None
-	p.axis.major_tick_line_color = None
-	p.axis.minor_tick_line_color = None
-	p.axis.major_label_text_font_size = "10pt"
-	p.axis.major_label_standoff = 0
-
-	p.rect(x="Year", y="Team", width=1, height=1,
-		source=source,
-		fill_color={'field': 'SumRV', 'transform': mapper},
-		line_color=None)
-
-	color_bar = ColorBar(color_mapper=mapper, major_label_text_font_size="5pt",
-		ticker=BasicTicker(desired_num_ticks=len(colors)),
-		formatter=PrintfTickFormatter(format="%d"),
-		label_standoff=6, border_line_color=None, location=(0,0))
-	p.add_layout(color_bar, 'right')
-
-	p.select_one(HoverTool).tooltips = [
-		('Team', '@Team'),
-		('Year', '@Year'),
-		('Total Value', '@SumRV'),
-	]
+	p = DraftHeatmap(choice)
 
 	script, div = components(p)
 
