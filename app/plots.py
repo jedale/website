2abf84fd06983548414af9fe84d14399671de2f1
Jeff Daley <daley.jeff@gmail.com>
2018-04-17 10:07:10 -0400

    reversed color on heatmap

--- app/plots.py
+++ app/plots.py
@@ -83,6 +83,7 @@ def DraftHeatmap(type):
 	# teams = list(df.Team.unique())
 
 	colors = ["#75968f", "#a5bab7", "#c9d9d3", "#e2e2e2", "#dfccce", "#ddb7b1", "#cc7878", "#933b41", "#550b1d"]
+	colors = list(reversed(colors))
 	mapper = LinearColorMapper(palette=colors, low=df.RV.min(), high=df.RV.max())
 
 	source = ColumnDataSource(df)
